aca11c126f11fc53d06affd03d1b3f0dba86ea4d
Leisure <675258207@qq.com>
2025-10-31 15:48:07 +0800

    feat: 支持错误自动重试

--- src/index.ts
+++ src/index.ts
@@ -39,11 +39,13 @@ export async function start({
 
     spainner.text = "当前选择月份";
     const { data: dateTime } = await getDateTime();
-    
+
     let recordDate = date || dateTime.substring(0, 7);
     console.log(chalk.green(`：${recordDate}`));
     spainner.text = "正在获取项目ID与项目名称";
-    const { id: projectId, name } = await getProjectId(recordDate.substring(0, 4));
+    const { id: projectId, name } = await getProjectId(
+      recordDate.substring(0, 4)
+    );
     console.log(chalk.green(`：${projectId}、${name}`));
 
     spainner.text = `正在获取${recordDate}的未完成报告数量`;
@@ -73,23 +75,48 @@ export async function start({
         }个。（一般一天2个，上午与下午各1个）`
       )
     );
-
+    let errorCount = 0;
     for (let i = 0; i < undoList.length; i++) {
-      spainner.text = `正在发布日报: ${i + 1}/${undoList.length}`;
+      if (errorCount === 0) {
+        spainner.text = `正在发布日报: ${i + 1}/${
+          undoList.length
+        } ${chalk.green(
+          `（${
+            undoList[i].start.substring(0, 10) + undoList[i].recordTypeStr
+          }）`
+        )}`;
+      }
+
       const undoItem = undoList[i];
       const recordDate = undoItem.start.substring(0, 10);
       const recordType: number = undoItem.recordTypeStr === "上午" ? 1 : 2;
       const itemContent = prefix
         ? `${recordDate}${undoItem.recordTypeStr}完成工作：\n${content}`
         : content;
-      await insertWorker({
-        userId: userInfo.userId,
-        deptIds: userInfo.deptIds,
-        projectId,
-        recordDate,
-        recordType,
-        content: itemContent,
-      });
+      try {
+        await insertWorker({
+          userId: userInfo.userId,
+          deptIds: userInfo.deptIds,
+          projectId,
+          recordDate,
+          recordType,
+          content: itemContent,
+        });
+      } catch (err: any) {
+        if (errorCount < 3) {
+          console.log(
+            chalk.red(
+              `\n发现错误：${err?.message},正在重试第${errorCount + 1}次`
+            )
+          );
+          errorCount++;
+          i--;
+          continue;
+        } else {
+          throw err;
+        }
+      }
+      errorCount = 0;
     }
     spainner.succeed(`已完成报告: ${undoList.length}个`);
     // console.log(chalk.green(`已完成日报: ${undoList.length}个`));
@@ -112,7 +139,9 @@ async function getProjectId(year: string) {
     userId: userInfo.userId,
   });
   if (res.status) {
-    const target = res.data.find((item: any) => item.name.includes("富民") && item.name.includes(year));
+    const target = res.data.find(
+      (item: any) => item.name.includes("富民") && item.name.includes(year)
+    );
     if (target) {
       return target;
     }
